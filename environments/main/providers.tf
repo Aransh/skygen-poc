24de2d396ed1614cb425073a9a6793e72207793f
Aransh <Aranshavit@gmail.com>
2023-08-24 22:00:09 +0300

    moved to remote state

--- environments/main/providers.tf
+++ environments/main/providers.tf
@@ -4,6 +4,12 @@ terraform {
       source  = "kreuzwerker/docker"
     }
   }
+  backend "s3" {
+    bucket = "hackaton-skygen-state"
+    key    = "skygen.tfstate"
+    region = "il-central-1"
+    skip_region_validation = true #Why is Tel-Aviv not recognized??
+  }
 }
 
 provider "docker" {
